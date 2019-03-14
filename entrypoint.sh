c3bf06460dc33e22bb966cfe02853e9036c116eb
flow.gunso <flow.gunso@gmail.com>
2019-03-14 23:31:19 +0100

    Sleep a bit more

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 # There's 5 second timeout to make sure the seafile deamon is properly initialised.
 
 /usr/bin/seaf-cli init -d /.seafile
+sleep 5
 /usr/bin/seaf-cli start
 sleep 5
 /usr/bin/seaf-cli sync -u $USERNAME -p $PASSWORD -s $SERVER -l $LIBRARY_ID -d /volume
+sleep 5
 /usr/bin/supervisord -u $UID -c /supervisord.conf
