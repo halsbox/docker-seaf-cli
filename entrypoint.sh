5a703261619dee4feb6c2a025295a648fd980e22
flow.gunso <flow.gunso@gmail.com>
2019-03-14 23:44:20 +0100

    Use $UNAME instead of $UID, try another way to make seafile syncing not fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# There's 5 second timeout to make sure the seafile deamon is properly initialised.
-
 /usr/bin/seaf-cli init -d /.seafile
-sleep 5
+while [ ! -f ~/.ccnet/seafile.ini ]; do sleep 1; done
 /usr/bin/seaf-cli start
-sleep 5
+while [ ! -f /.seafile/seafile-data/seafile.sock ]; do sleep 1; done
 /usr/bin/seaf-cli sync -u $USERNAME -p $PASSWORD -s $SERVER -l $LIBRARY_ID -d /volume
-sleep 5
-/usr/bin/supervisord -u $UID -c /supervisord.conf
+/usr/bin/supervisord -u $UNAME -c /supervisord.conf
