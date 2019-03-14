f8d63f93008329cf1121bd58656bbb8ff56c3997
flow.gunso <flow.gunso@gmail.com>
2019-03-14 23:54:56 +0100

    Wait for a socket

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 /usr/bin/seaf-cli init -d /.seafile
 while [ ! -f ~/.ccnet/seafile.ini ]; do sleep 1; done
 /usr/bin/seaf-cli start
-while [ ! -f /.seafile/seafile-data/seafile.sock ]; do sleep 1; done
+while [ ! -S /.seafile/seafile-data/seafile.sock ]; do sleep 1; done
 /usr/bin/seaf-cli sync -u $USERNAME -p $PASSWORD -s $SERVER -l $LIBRARY_ID -d /volume
 /usr/bin/supervisord -u $UNAME -c /supervisord.conf
