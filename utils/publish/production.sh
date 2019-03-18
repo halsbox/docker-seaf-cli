ca8ecfaeb455eecc1e77cbbe3071c9ab41a857ab
flow.gunso <flow.gunso@gmail.com>
2019-03-18 19:10:05 +0100

    Add GPLv3 headers

--- utils/publish/production.sh
+++ utils/publish/production.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Docker Seafile client, help you mount a Seafile library as a volume.
+# Copyright (C) 2019, flow.gunso@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Restrict production build to tag matching MAJOR.MINOR.REVISION.
 if ! [[ "$CI_COMMIT_TAG" =~ ^[0-9]+[.][0-9]+[.][0-9]+$ ]]; then
     echo "Version number must match major.minor.revision!"
