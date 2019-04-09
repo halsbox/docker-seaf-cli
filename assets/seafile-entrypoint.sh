4320e5b85be318b0fc6b69b49d80c2e842fefa03
flow.gunso <flow.gunso@gmail.com>
2019-04-09 18:21:27 +0200

    Unset -x

--- assets/seafile-entrypoint.sh
+++ assets/seafile-entrypoint.sh
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-set -x
-
 # Define variable shortcuts for readability purposes.
 seafile_ini=~/.ccnet/seafile.ini
 seafile_sock=~/.seafile/seafile-data/seafile.sock
