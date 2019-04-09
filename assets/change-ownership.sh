e2fcfcee053017c93675f98e80a9ae8327889770
flow.gunso <flow.gunso@gmail.com>
2019-04-09 15:00:11 +0200

    Drop the script now useless

--- assets/change-ownership.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-groupmod -u $GID $UNAME
-usermod -u $UID $UNAME
-chown $UID.$GID -R /.seafile
-chown $UID.$GID -R /.supervisord
-chown $UID.$GID -R /volume
-chown $UID.$GID /entrypoint.sh
-chown $UID.$GID /infinite-seaf-cli-start.sh
