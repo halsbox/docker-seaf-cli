8a0225edeb8f277adf29b4c4fa8b94859714b2bc
flow.gunso <flow.gunso@gmail.com>
2019-04-09 12:00:42 +0200

    Update user/groud ID, fix the script

--- assets/change-ownership.sh
+++ assets/change-ownership.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-groupadd -g $GID -o $UNAME ;\
-useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME ;\
-chown $UID.$GID -R /.seafile ;\
-chown $UID.$GID -R /.supervisord ;\
-chown $UID.$GID -R /volume ;\
-chown $UID.$GID /entrypoint.sh ;\
+groupmod -u $GID $UNAME
+usermod -u $UID $UNAME
+chown $UID.$GID -R /.seafile
+chown $UID.$GID -R /.supervisord
+chown $UID.$GID -R /volume
+chown $UID.$GID /entrypoint.sh
 chown $UID.$GID /infinite-seaf-cli-start.sh
