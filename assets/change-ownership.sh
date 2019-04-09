891d34aad168862120ef7e55fe14327e6594e9e4
flow.gunso <flow.gunso@gmail.com>
2019-04-09 11:46:40 +0200

    UID and GID are always set

--- assets/change-ownership.sh
+++ assets/change-ownership.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ -z $UID ] && [ -z $GID ]; then
-    groupadd -g $GID -o $UNAME ;\
-    useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME ;\
-    chown $UID.$GID -R /.seafile ;\
-    chown $UID.$GID -R /.supervisord ;\
-    chown $UID.$GID -R /volume ;\
-    chown $UID.$GID /entrypoint.sh ;\
-    chown $UID.$GID /infinite-seaf-cli-start.sh
-else
-    echo "\$UID and \$GID environment variables are required to update files ownership."
-fi
+groupadd -g $GID -o $UNAME ;\
+useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME ;\
+chown $UID.$GID -R /.seafile ;\
+chown $UID.$GID -R /.supervisord ;\
+chown $UID.$GID -R /volume ;\
+chown $UID.$GID /entrypoint.sh ;\
+chown $UID.$GID /infinite-seaf-cli-start.sh
