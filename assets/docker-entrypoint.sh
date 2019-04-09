e23d93338e302138599bb4de368dc36e2998d8c6
flow.gunso <flow.gunso@gmail.com>
2019-04-09 16:36:53 +0200

    Preserve environment when starting Seafile

--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -44,4 +44,4 @@ chown $UID.$GID -R /home/seafuser/infinite-seaf-cli-start.sh
 chown $UID.$GID -R /home/seafuser/entrypoint.sh
 
 # Run the Seafile client as the container user.
-su - $UNAME -c "/bin/bash /home/seafuser/entrypoint.sh"
+su - $UNAME -m -c "/bin/bash /home/seafuser/entrypoint.sh"
